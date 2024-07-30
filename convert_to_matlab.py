56cb90a72c323f855f6ddbe6b7b687951eeb88dc
Spencer H <SpencerHanson3141@gmail.com>
2024-07-29 21:47:06 -0600

    Update convert_to_matlab.py

--- convert_to_matlab.py
+++ convert_to_matlab.py
@@ -1,13 +1,30 @@
 import pickle
 import os
+
+import numpy as np
 import scipy
 
 
 def convert_to_matlab(filename, output_filename):
-    print(f"Converting '{filename}' to '{output_filename}'")
-    with open(filename, "rb") as f:
-        pkl = pickle.load(f)
-        scipy.io.savemat(output_filename, pkl)
+    try:
+        with open(filename, "rb") as f:
+            print(f"Converting '{filename}' to '{output_filename}'")
+            pkl = pickle.load(f)
+            if filename == "all-units.pickle":
+                all_units_dict = {}
+                for key in pkl[0].keys():
+                    if key not in all_units_dict:
+                        all_units_dict[key] = []
+                    for item in pkl:
+                        all_units_dict[key].append(item[key])
+                scipy.io.savemat(output_filename, all_units_dict)
+            else:
+                scipy.io.savemat(output_filename, pkl)
+    except Exception as e:
+        raise e
+        with open(f"{output_filename}-error.txt", "w") as f2:
+            f2.write(str(e))
+        print(f"Error! Skipping '{str(e)}'")
 
 
 def main():
@@ -25,10 +42,11 @@ def main():
                 save_fn = filename
             save_fn = save_fn[:-len(".pickle")] + ".mat"
 
-            if os.path.exists(save_fn):
-                print(f"File '{save_fn}' already exists, skipping..")
+            save_path = os.path.join(output_folder, save_fn)
+            if os.path.exists(save_path):
+                print(f"File '{save_path}' already exists, skipping..")
             else:
-                convert_to_matlab(filename, os.path.join(output_folder, save_fn))
+                convert_to_matlab(filename, save_path)
 
     tw = 2
 
